4fd4f11b3f1b197082d3f24ce7afffa7c804166f
adrianocruzweb <adrianocruzweb@hotmail.com>
2022-07-03 21:18:34 -0400

    Adicionar arquivos de projeto.

---
+++ WebApplication1/Controllers/HelloController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
